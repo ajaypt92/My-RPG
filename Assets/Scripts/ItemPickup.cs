5b08be176b4fc3d41b28d19ad9bbc3a197a89e9c
ajaypt92 <ajaypt92@gmail.com>
2018-08-30 00:52:01 -0400

    FIrst
    Awesome

---
+++ Assets/Scripts/ItemPickup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : Interactable {
+
+    public Item item;	// Item to put in the inventory on pickup
+
+    public override void Interact()
+    {
+        base.Interact();
+
+        // Function to pickup the item and add to inventory
+        PickUp();
+    }
+
+    public void PickUp()
+    {
+        Debug.Log("Picking up " + item.name);
+        // Code to add the item to the inventory
+
+        bool wasPickedUp = Inventory.instance.Add(item);    // Add to inventory
+
+        // If successfully picked up
+        if (wasPickedUp)
+            Destroy(gameObject);	// Destroy item from scene
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+
+    public LayerMask movementMask;
+
+    Camera cam;
+    PlayerMotor motor;
+
+    public Interactable focus;
+
+	// Use this for initialization
+	void Start () {
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(EventSystem.current.IsPointerOverGameObject())
+        {
+            return;
+        }
+
+        // If Left mouse button clicked - move to a point
+		if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, movementMask))
+            {
+                // Move our player to where we hit
+                motor.MoveToPoint(hit.point);
+
+                // Stop focusing on the current obect
+                RemoveFocus();
+            }
+        }
+
+        // If Right mouse button clicked - focus on an interactable object
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {                
+                // Check if we clicked on an interactable
+                Interactable interactable = hit.collider.GetComponent<Interactable>();
+                // If we did, set it as our focus
+                if (interactable != null)
+                {
+                    SetFocus(interactable);
+                }
+            }
+        }
+    }
+
+    // Set our focus to a new focus
+
+    void SetFocus(Interactable newFocus)
+    {
+        // If our focus has changed
+        if (newFocus != focus)
+        {
+            // Defocus the old one
+            if (focus != null)
+                focus.OnDefocused();
+
+            focus = newFocus;   // Set our new focus
+            motor.FollowTarget(newFocus);   // Follow the new focus
+        }
+        newFocus.OnFocused(transform);
+    }
+
+
+
+    // Remove our current focus
+    void RemoveFocus()
+    {
+        if (focus != null)
+            focus.OnDefocused();
+
+        focus = null;
+        motor.StopFollowingTarget();
+    }
+}
